61b87445f0aa8338ae9163d490a813e38f81474d
Wilondja243 <lucksonpremier@gmail.com>
2025-12-15 16:08:47 +0200

    change conf file

--- shotel/wsgi.py
+++ shotel/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shotel.config.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shotel.config.production')
 
 application = get_wsgi_application()
