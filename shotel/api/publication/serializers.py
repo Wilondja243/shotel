679fe94409ae059056da6723d2fe50e6261770ed
Wilondja243 <lucksonpremier@gmail.com>
2025-12-24 18:03:03 +0200

    allow_null to base64 fields

--- shotel/api/publication/serializers.py
+++ shotel/api/publication/serializers.py
@@ -10,8 +10,8 @@ from shotel.app.publication.models import Post, Comment
 class PostSerializer(serializers.ModelSerializer):
     author = UserMiniSerializer(read_only=True)
     likes_count = serializers.SerializerMethodField(read_only=True)
-    image = Base64ImageField(required=False)
-    video = Base64FileField(required=False)
+    image = Base64ImageField(required=False, allow_null=True)
+    video = Base64FileField(required=False, allow_null=True)
 
     class Meta:
         model = Post
